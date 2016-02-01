15601e0a589606d5db7895f8e87934d1e151da75
Kyungwook Tak <k.tak@samsung.com>
2016-02-01 16:56:17 +0900

    Refactor build system and add internal test
    * Refine cmake files
    * Remove duplicated codes in duid-gadget
    * Add internal test
    
    Change-Id: I5cc7e16db85cc734d65794f8a505bcdd1dfd8726
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

--- gadget/duid-gadget.c
+++ gadget/duid-gadget.c
@@ -1,6 +1,5 @@
 /*
- *
- * Copyright (c) 2000 - 2013 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2000 - 2016 Samsung Electronics Co., Ltd All Rights Reserved
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,56 +17,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include <openssl/evp.h>
-#include <openssl/rand.h>
 
 #include <SecCryptoSvc.h>
 
 int main()
 {
-	const char *version = "1.0#";
-	char info[] = {0xca, 0xfe, 0xbe, 0xbe, 0x78, 0x07, 0x02, 0x03};
+	char *duid = GetDuid(20);
 
-	int ret = 0;
-	int keyLen = 20;
-	unsigned char *pKey = NULL;
-	unsigned char *pDuid = NULL;
-	char *pId = NULL;
-	char *pKeyVersion = NULL;
-
-	if (!(pKey = (unsigned char *)malloc(keyLen)))
-		goto exit;
-
-	if (!SecFrameGeneratePlatformUniqueKey(keyLen, pKey)) {
+	if (duid == NULL) {
 		fprintf(stderr, "Failed to get duid\n");
-		goto exit;
+		return 0;
 	}
 
-	if (!(pDuid = (unsigned char *)malloc(keyLen)))
-		goto exit;
+	printf("%s", duid);
 
-	PKCS5_PBKDF2_HMAC_SHA1(info, 8, pKey, keyLen, 1, keyLen, pDuid);
+	free(duid);
 
-	if (!(pId = Base64Encoding((char *)pDuid, keyLen)))
-		goto exit;
-
-	if (!(pKeyVersion = (char *)calloc(strlen(pId) + strlen(version) + 1, sizeof(char))))
-		goto exit;
-
-	strncpy(pKeyVersion, version, strlen(version));
-	strncat(pKeyVersion, pId, strlen(pId));
-	printf("%s", pKeyVersion);
-
-	ret = 1;
-
-exit:
-	free(pKey);
-	free(pDuid);
-	free(pId);
-	free(pKeyVersion);
-
-	return ret;
+	return 1;
 }
-
