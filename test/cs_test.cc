d6af2fe1baed9c782fc492936718ed6d0303bbee
Kyungwook Tak <k.tak@samsung.com>
2016-02-02 16:32:04 +0900

    Integrate dukgenerator API to cryptsvc
    Change-Id: Idfefdf9444a69fa7b9f18a2b249809f7abc89ad3
    Signed-off-by: Kyungwook Tak <k.tak@samsung.com>

--- test/cs_test.cc
+++ test/cs_test.cc
@@ -20,6 +20,9 @@
  * @version
  * @brief
  */
+
+#include <stdlib.h>
+#include <memory>
 #include <iostream>
 
 #include <boost/test/unit_test.hpp>
@@ -32,33 +35,63 @@ BOOST_AUTO_TEST_CASE(PLATFORM_UNIQUE_KEY)
 {
     constexpr unsigned int KeyLen = 16;
     unsigned char cek[KeyLen] = {0};
-    char *encoded_cek = nullptr;
 
     BOOST_REQUIRE_MESSAGE(SecFrameGeneratePlatformUniqueKey(KeyLen, cek),
         "Failed to SecFrameGeneratePlatformUniqueKey.");
 
-    encoded_cek = Base64Encoding(
-            reinterpret_cast<char *>(cek),
-            static_cast<int>(KeyLen));
-    BOOST_REQUIRE_MESSAGE(encoded_cek != nullptr, "Failed to base64 encoding.");
+    std::unique_ptr<char, void(*)(void *)> encoded_cek(
+        Base64Encoding(reinterpret_cast<char *>(cek), static_cast<int>(KeyLen)),
+        free);
+    BOOST_REQUIRE_MESSAGE(!!encoded_cek, "Failed to base64 encoding.");
 
     std::cout << "base64 encoded platform unique key(with len 16): "
-        << encoded_cek << std::endl;
+        << encoded_cek.get() << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(GETDUID_16)
 {
-    char *duid = GetDuid(16);
-    BOOST_REQUIRE_MESSAGE(duid != nullptr, "returned duid shouldn't be null");
-    std::cout << "duid: " << duid << std::endl;
+    std::unique_ptr<char, void(*)(void *)> duid(GetDuid(16), free);
+    BOOST_REQUIRE_MESSAGE(!!duid, "returned duid shouldn't be null");
+    std::cout << "duid: " << duid.get() << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(GETDUID_20)
 {
-    char *duid = GetDuid(20);
-    BOOST_REQUIRE_MESSAGE(duid != nullptr, "returned duid shouldn't be null");
-    std::cout << "duid: " << duid << std::endl;
+    std::unique_ptr<char, void(*)(void *)> duid(GetDuid(20), free);
+    BOOST_REQUIRE_MESSAGE(!!duid, "returned duid shouldn't be null");
+    std::cout << "duid: " << duid.get() << std::endl;
 }
 
+static void derive_key_with_pass(const char *pass, int passlen)
+{
+    int retval = CS_ERROR_NONE;
+
+    constexpr unsigned int KeyLen = 20;
+    unsigned char *key = nullptr;
+    BOOST_REQUIRE_MESSAGE(
+        (retval = cs_derive_key_with_pass(pass, passlen, KeyLen, &key)) == CS_ERROR_NONE,
+        "Failed to cs_derive_key_with_pass with retval: " << retval);
+
+    std::unique_ptr<unsigned char, void(*)(void *)> pKey(key, free);
+
+    std::unique_ptr<char, void(*)(void *)> encoded_key(
+        Base64Encoding(reinterpret_cast<char *>(key), static_cast<int>(KeyLen)),
+        free);
+    BOOST_REQUIRE_MESSAGE(!!encoded_key, "Failed to base64 encoding.");
+
+    std::cout << "base64 encoded key derived from pass(len " << KeyLen
+        << "): " << encoded_key.get() << std::endl;
+}
+
+BOOST_AUTO_TEST_CASE(DERIVE_KEY_WITH_PASS)
+{
+    const char *test_pass = "test-password";
+    derive_key_with_pass(test_pass, 5);
+    derive_key_with_pass(test_pass, 10);
+    derive_key_with_pass(test_pass, strlen(test_pass));
+
+    const char empty_pass[30] = {0, };
+    derive_key_with_pass(empty_pass, strlen(empty_pass));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
